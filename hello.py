2bf577e015d552500ceb28a4eca837eb04729991
Emin Can Işık <canemin0@hotmail.com>
2025-03-10 15:36:38 +0300

    python

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Lütfen Adınızı Giriniz: ")
+name = input()
+print("Lütfen Soyadınızı Giriniz: ")
+lastName = input()
+print("Lütfen Yaşınızı Giriniz: ")
+age = int(input())
+
+print(f"Merhaba {name} {lastName}, {age} yaşındasınız. Hoş Geldiniz!")
